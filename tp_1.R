249c620818af8073449276137d49f0e12f986283
MANTINOU <123742787+MANTINOU@users.noreply.github.com>
2023-01-30 23:00:31 +0100

    Update tp_1.R

--- tp_1.R
+++ tp_1.R
@@ -1,1 +1,11 @@
-y = (2,5)
+heure_semaine <- as.numeric(readline())
+28
+taux_horaire <- as.numeric(readline())
+20
+nbre_semaine <- as.numeric(readline())
+52
+mois <- 12
+total_heure <- heure_semaine*nbre_semaine/mois
+salair_brut <- total_heure*taux_horaire
+taux <- 0.23
+salair_net <- salair_brut- (salair_brut*taux)
